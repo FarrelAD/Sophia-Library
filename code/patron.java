423b9ae0a6e407bd3767a9779a266d4df3d04f35
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-01-30 22:16:37 +0700

    add submenu function of patron user

--- code/patron.java
+++ code/patron.java
@@ -11,7 +11,7 @@ public class patron {
         System.out.print(
             "-- PATRON MENU --\n" +
             "   1. BORROW BOOKS\n" +
-            "   2. SEARCH BOOKS\n" +
+            "   2. SEARCH COLLECTIONS\n" +
             "   3. EXTEND LOAN PERIOD\n" +
             "   99. BACK\n" +
             "   00. EXIT\n" +
@@ -19,5 +19,43 @@ public class patron {
         );
         userInputMenu = scan1.nextInt();
         scan1.nextLine();
+
+        if (userInputMenu == 1) {
+            borrowBooksMenu();
+        } else if (userInputMenu == 2) {
+            searchCollectionsMenu();
+        } else if (userInputMenu == 3) {
+            extendLoanPeriod();
+        } else if (userInputMenu == 99) {
+            main.login();
+        } else if (userInputMenu == 00) {
+            main.exit();
+        } else {
+            main.displayInvalidInput();
+        }
+    }
+
+    private static void borrowBooksMenu() {
+        main.clearScreen();
+
+        System.out.println(
+            "-- BORROW BOOKS MENU --"
+        );
+    }
+
+    private static void searchCollectionsMenu() {
+        main.clearScreen();
+
+        System.out.println(
+            "-- SEARCH LIBRARY COLLECTIONS MENU --"
+        );
+    }
+
+    private static void extendLoanPeriod() {
+        main.clearScreen();
+
+        System.out.println(
+            "-- EXTEND LOAN PERIOD MENU --"
+        );
     }
 }
